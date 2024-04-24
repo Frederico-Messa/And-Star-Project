d158b22a081dbc97cbb8875adebaca9a19e3f756
Frederico Messa <frederico.messa@gmail.com>
2024-04-24 19:27:00 -0300

    remove files from previous version

--- python_parser/atomic_effect.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from python_parser.variable import Variable
-from python_parser.partial_state import PartialState
-
-@dataclass(eq=True, frozen=True)
-class AtomicEffect:
-    condition: PartialState
-    affected_variable: Variable
-    assignment_value: int
--- python_parser/effect.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-from python_parser.atomic_effect import AtomicEffect
-
-@dataclass(eq=True, frozen=True)
-class Effect:
-    atomic_effects: frozenset[AtomicEffect]
